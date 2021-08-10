67d3d29c1a5789b62cf6dec5b952069a292170ca
Pablo <superpato1234@hotmail.com>
2021-08-10 14:37:09 -0500

    Strings

---
+++ String.py
@@ -0,0 +1,16 @@
+my_string= "Hola como estan?"
+
+#print (dir(my_string))
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.swapcase())
+print(my_string.capitalize())
+print(my_string.replace("estan?","amanecieron?"))
+print(my_string.count('a'))
+print(my_string.startswith("Hola"));
+print(my_string.endswith("?"))
+print(my_string.split("a"))
+print(my_string.find("?"))
+print(my_string.index("c"))
+print(len(my_string))
--- variables.py
+++ variables.py
@@ -7,3 +7,13 @@ x1,x2= "jojojo",123123
 
 print (var, Book, book, x1, x2)
 
+#conventions
+
+book_name= "harry potter"   #snake case
+bookName= "harry potter"    #camel case
+BookName= "harry potter"    #pascal case
+
+#constant
+PI = 3.141516
+MY_NAME= "Pablito"
+
